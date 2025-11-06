67b040f9904045c26985419a9a7ce65116693690
UdayDabi <144447062+UdayDabi@users.noreply.github.com>
2025-11-06 15:41:07 +0530

    Update list_second_largest.py

--- pythonProject/exercise/list_second_largest.py
+++ pythonProject/exercise/list_second_largest.py
@@ -14,3 +14,15 @@ for num in numbers:
 
 print("Highest number is:", highest)
 print("Second highest number is:", second_highest)
+
+
+
+# numbers = [100, 10, 11, 5, 13, 17, 88]
+
+# highest = numbers[0]   # pehle element ko hi highest maan lo
+
+# for num in numbers:
+#     if num > highest:
+#         highest = num
+
+# print("Highest number is:", highest)
