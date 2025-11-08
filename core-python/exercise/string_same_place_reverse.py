249706c117336183e501afb4e15019f35183d802
UdayDabi <144447062+UdayDabi@users.noreply.github.com>
2025-11-08 13:01:16 +0530

    Update string_same_place_reverse.py

--- core-python/exercise/string_same_place_reverse.py
+++ core-python/exercise/string_same_place_reverse.py
@@ -6,3 +6,13 @@ for word in word_list:
     for char in word:
         reversed_word = char + reversed_word
     print(reversed_word, end=" ")
+
+
+# str1 = input("Enter a string: ")
+# count = 0
+
+# for ch in str1:
+#     if ch.lower() in ['a', 'e', 'i', 'o', 'u']:
+#         count += 1
+
+# print("Total vowels =", count)
