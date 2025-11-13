df401524b28a2f5abf6b88d2061020278e0140ae
UdayDabi <144447062+UdayDabi@users.noreply.github.com>
2025-11-13 15:39:22 +0530

    Update protected_variables.py

--- core-python/oop-private-protected/protected_variables.py
+++ core-python/oop-private-protected/protected_variables.py
@@ -27,3 +27,39 @@ print("Border Width:", s.getBorderWidth())
 
 # Accessing the protected variables directly (not recommended, but possible)
 print("Protected color directly:", s._color)  # This is allowed but not encouraged
+
+
+
+
+
+
+
+
+
+
+# class Account:
+#     def __init__(self):
+#         self.__balance = 5000     # Private variable
+#         self._accountType = "Saving"  # Protected variable
+
+#     def show(self):
+#         print("Balance (Private):", self.__balance)
+#         print("Account Type (Protected):", self._accountType)
+
+
+# class ChildAccount(Account):
+#     def display(self):
+#         # print(self.__balance)   ❌ Not allowed (Private)
+#         print("Accessing protected:", self._accountType)  # ✅ Allowed
+
+
+# # ---------------- Example Use ----------------
+# a = Account()
+# a.show()
+
+# child = ChildAccount()
+# child.display()
+
+# # Try accessing directly
+# # print(a.__balance)   ❌ Error (private)
+# print(a._accountType)  # ⚠️ Allowed but not recommended
